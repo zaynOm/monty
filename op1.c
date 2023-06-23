d94994cafabc7b9b54110b3717e326fbb966d37d
zaynOm <ouaziz0626@gmail.com>
2023-06-23 02:06:28 +0100

    fix

---
+++ op1.c
@@ -0,0 +1,59 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack.
+ * @top: top of the stack
+ * @line_number: current line number in the file
+*/
+void push(stack_t **top, ui line_number)
+{
+	stack_t *new;
+	(void) line_number;
+
+	new = malloc(sizeof(stack_t));
+	if (!new)
+		simple_error(MALLOC_FIALED);
+	/* *new = (stack_t) {.n = num, .next = NULL, .prev = NULL}; */
+	new->n = num;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (!*top)
+		*top = new;
+	else
+	{
+		new->next = *top;
+		(*top)->prev = new;
+		*top = new;
+	}
+}
+
+/**
+ * pall - prints all the values on the stack, starting from the top
+ * @top: top of the stack
+ * @line_number: current line number in the file
+*/
+void pall(stack_t **top, ui line_number)
+{
+	stack_t *tmp = *top;
+	(void) line_number;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
+
+/**
+ * pint -  prints the value at the top of the stack
+ * @top: top of the stack
+ * @line_number: current line number in the file
+ */
+void pint(stack_t **top, ui line_number)
+{
+	if (!*top)
+		op_error(CANT_PRINT_EMPTY_STACK, line_number);
+
+	printf("%d\n", (*top)->n);
+}
