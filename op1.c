79819b7fba164900bdd207033a5864e35d7323d9
zaynOm <ouaziz0626@gmail.com>
2023-06-23 02:07:32 +0100

    fix

--- op1.c
+++ op1.c
@@ -4,7 +4,7 @@
  * push - pushes an element to the stack.
  * @top: top of the stack
  * @line_number: current line number in the file
-*/
+ */
 void push(stack_t **top, ui line_number)
 {
 	stack_t *new;
@@ -32,7 +32,7 @@ void push(stack_t **top, ui line_number)
  * pall - prints all the values on the stack, starting from the top
  * @top: top of the stack
  * @line_number: current line number in the file
-*/
+ */
 void pall(stack_t **top, ui line_number)
 {
 	stack_t *tmp = *top;
