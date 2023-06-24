f5b65bd96d199cdccae097c2c7a41bc49a1c2810
zaynOm <ouaziz0626@gmail.com>
2023-06-25 00:40:29 +0100

    nop :fire:

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @top: pointer to the head of the stack
+ * @line_number: line number of the command being run
+ */
+void nop(stack_t **top, uint line_number)
+{
+	(void)top;
+	(void)line_number;
+}
