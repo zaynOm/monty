8818e9c4ff0e04b4519b860dbe76d3a01c9cee9d
zaynOm <ouaziz0626@gmail.com>
2023-06-25 00:43:37 +0100

    adding freeall

--- stack_utils.c
+++ stack_utils.c
@@ -15,3 +15,15 @@ void free_stack(stack_t **head)
 		free(temp);
 	}
 }
+
+/**
+ * freeall - frees all allocated memory
+ * @exit_code: the exit code
+*/
+void freeall(int exit_code)
+{
+	fclose(data.f);
+	free(data.line);
+	free_stack(&data.top);
+	exit(exit_code);
+}
